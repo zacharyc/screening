63484cc2048d946e0f52583f848ac062272c9a7a
Zachary Cohen <zachary.cohen@motorola.com>
2012-06-05 18:15:17 -0700

    Updating connect script

--- server/client/connect.js
+++ server/client/connect.js
@@ -4,17 +4,16 @@ console.log(webdriver);
 
 setTimeout(function() {
     console.log('timeout');
-    debugger;
     //webdriver.chrome.driver = true; TODOz: seems to be from Java
-    var client = new webdriver.http.CorsClient('http://localhost:9515/');
+    var client = new webdriver.http.CorsClient('http://localhost:4444/wd/hub');
     var executor = new webdriver.http.Executor(client);
 
     // Launches a new browser, which can be controlled by this script.
     var driver = webdriver.WebDriver.createSession(executor, {
-    'browserName': 'chrome',
-    'version': '',
-    'platform': 'ANY',
-    'javascriptEnabled': true
+        'browserName': 'chrome',
+        'version': '',
+        'platform': 'ANY',
+        'javascriptEnabled': true
     });
 
     //driver.setWindowSize(20,20);
@@ -22,10 +21,10 @@ setTimeout(function() {
     driver.findElement(webdriver.By.name('q')).sendKeys('webdriver');
     driver.findElement(webdriver.By.name('btnG')).click();
     driver.getTitle().then(function(title) {
-    if (title !== 'webdriver - Google Search') {
-      throw new Error(
-          'Expected "webdriver - Google Search", but was "' + title + '"');
-    }
+        if (title !== 'webdriver - Google Search') {
+            throw new Error(
+                'Expected "webdriver - Google Search", but was "' + title + '"');
+        }
     });
 
     driver.quit();
