60ade386272d13d9e5f794c13f0f16b7881c89cd
Zachary Cohen <zachary.cohen@motorola.com>
2012-06-06 11:38:14 -0700

    Getting test to pass in WD
    Needed to do a then

--- server/client/connect.js
+++ server/client/connect.js
@@ -3,6 +3,7 @@
 console.log(webdriver);
 
 setTimeout(function() {
+    debugger;
     console.log('timeout');
     //webdriver.chrome.driver = true; TODOz: seems to be from Java
     var client = new webdriver.http.CorsClient('http://localhost:4444/wd/hub');
@@ -19,13 +20,15 @@ setTimeout(function() {
     //driver.setWindowSize(20,20);
     driver.get('http://www.google.com');
     driver.findElement(webdriver.By.name('q')).sendKeys('webdriver');
-    driver.findElement(webdriver.By.name('btnG')).click();
-    driver.getTitle().then(function(title) {
-        if (title !== 'webdriver - Google Search') {
-            throw new Error(
-                'Expected "webdriver - Google Search", but was "' + title + '"');
-        }
+    driver.findElement(webdriver.By.name('btnG')).click().then(function() {
+        driver.getTitle().then(function(title) {
+            if (title !== 'webdriver - Google Search') {
+                throw new Error(
+                    'Expected "webdriver - Google Search", but was "' + title + '"');
+            }
+        });
     });
+    
 
     driver.quit();
 
