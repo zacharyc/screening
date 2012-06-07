155e0398e93b012c75cf8b48fcd8d4e6a59fe481
Zachary Cohen <zachary.cohen@motorola.com>
2012-06-06 17:44:01 -0700

    Updating connect script
    Still in test

--- server/client/connect.js
+++ server/client/connect.js
@@ -1,35 +1,30 @@
 //alert('connect');
 
-console.log(webdriver);
+console.log('timeout');
+//webdriver.chrome.driver = true; TODOz: seems to be from Java
+var client = new webdriver.http.CorsClient('http://localhost:4444/wd/hub');
+var executor = new webdriver.http.Executor(client);
 
-setTimeout(function() {
-    debugger;
-    console.log('timeout');
-    //webdriver.chrome.driver = true; TODOz: seems to be from Java
-    var client = new webdriver.http.CorsClient('http://localhost:4444/wd/hub');
-    var executor = new webdriver.http.Executor(client);
+// Launches a new browser, which can be controlled by this script.
+var driver = webdriver.WebDriver.createSession(executor, {
+    'browserName': 'chrome',
+    'version': '',
+    'platform': 'ANY',
+    'javascriptEnabled': true
+});
 
-    // Launches a new browser, which can be controlled by this script.
-    var driver = webdriver.WebDriver.createSession(executor, {
-        'browserName': 'chrome',
-        'version': '',
-        'platform': 'ANY',
-        'javascriptEnabled': true
-    });
+//driver.setWindowSize(300,300);
+driver.get('http://localhost:8081/samples/sample.html');
+//driver.get('http://www.google.com');
+// driver.findElement(webdriver.By.name('q')).sendKeys('webdriver');
+// driver.findElement(webdriver.By.name('btnG')).click().then(function() {
+//     driver.getTitle().then(function(title) {
+//         if (title !== 'webdriver - Google Search') {
+//             throw new Error(
+//                 'Expected "webdriver - Google Search", but was "' + title + '"');
+//         }
+//     });
+// });
 
-    //driver.setWindowSize(20,20);
-    driver.get('http://www.google.com');
-    driver.findElement(webdriver.By.name('q')).sendKeys('webdriver');
-    driver.findElement(webdriver.By.name('btnG')).click().then(function() {
-        driver.getTitle().then(function(title) {
-            if (title !== 'webdriver - Google Search') {
-                throw new Error(
-                    'Expected "webdriver - Google Search", but was "' + title + '"');
-            }
-        });
-    });
-    
 
-    driver.quit();
-
-}, 4000);
+//driver.quit();
